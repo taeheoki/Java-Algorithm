72cac19b19c5bbe27014998d0422ef044376400d
taeheoki <dkqjwl2365@naver.com>
2023-10-13 23:58:12 +0900

    🧑🏻‍💻feat: 교수가 된 현우

--- src/week_41/BOJ3474.java
+++ src/week_41/BOJ3474.java
@@ -1,4 +1,27 @@
 package src.week_41;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class BOJ3474 {
+    static StringBuilder sb = new StringBuilder();
+    static int T;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        T = Integer.parseInt(br.readLine());
+        while (T-- > 0) {
+            int num = Integer.parseInt(br.readLine());
+            pro(num);
+        }
+        System.out.println(sb);
+    }
+
+    private static void pro(int num) {
+        long fiveCnt = 0;
+        for (long i = 5; i <= num; i *= 5)
+            fiveCnt += num / i;
+        sb.append(fiveCnt).append('\n');
+    }
 }
