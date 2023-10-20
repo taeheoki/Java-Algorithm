8382a40c93a649a55183ee79704f3c319eb90644
taeheoki <dkqjwl2365@naver.com>
2023-10-20 17:34:38 +0900

    🧑🏻‍💻feat: 감소하는 수

--- src/week_42/BOJ1038.java
+++ src/week_42/BOJ1038.java
@@ -3,24 +3,40 @@ package src.week_42;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
 
 /**
  * BOJ 1038 - 감소하는 수
  */
 public class BOJ1038 {
-    static int N, rank;
+    static int N;
+    static ArrayList<Long> list = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        rec_func(0, 0);
+
+        if (N <= 10) {
+            System.out.println(N);
+        }
+        else if (N > 1022) {
+            System.out.println(-1); // 감소하는 수는 최대 1022개
+        } else {
+            for (int i = 0; i <= 9; i++) {
+                makeDescNum(i, 1);
+            }
+            Collections.sort(list);
+            System.out.println(list.get(N));
+        }
     }
 
-    private static void rec_func(int rank, int num) {
-        if (rank == num) {
-            System.out.println(num);
-            System.exit(0);
+    private static void makeDescNum(long num, int depth) {
+        if (depth > 10)
+            return;
+        list.add(num);
+        for (int i = 0; i < num % 10; i++) {
+            makeDescNum(num * 10 + i, depth + 1);
         }
-
     }
 }
